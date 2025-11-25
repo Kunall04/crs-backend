ae2ba51ed1fba8ecdd58e867af6c031e398d7f4c
Kunal Rohilla <93761501+Kunall04@users.noreply.github.com>
2025-11-25 23:54:45 +0530

    mnr chngs

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,22 @@
 const { Router } = require('express');
 const userRouter = Router();
 
-userRouter.get('/signup',function(req,res){
+userRouter.post('/signup', async (req, res) => {
+    try {
+        const { email, password, firstName, lastName } = req.body;
+        if (!email || !password) 
+            return res.status(400).json({ 
+                message: 'email and password required' 
+            });
 
-    res.json({
-        message: "signup"
-    })
-}); 
+        res.status(201).json({ 
+            id: user._id, email: user.email 
+        });
+        
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
 
 module.exports={
     userRouter: userRouter
