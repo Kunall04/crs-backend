cc4f1b8006974087f0aacc4ba213c3bbe1e5dbb4
Kunal Rohilla <93761501+Kunall04@users.noreply.github.com>
2025-11-27 23:53:51 +0530

    mnr chngs

--- routes/course.js
+++ routes/course.js
@@ -27,7 +27,16 @@ courseRouter.post('/purchases', async function (req, res) {
     }
 });
 
-
+// list purchases for current user
+courseRouter.get('/purchases', async function (req, res) {
+    try {
+        const userId = req.userId;
+        const purchases = await PurchaseModel.find({ userId }).populate('courseId');
+        res.json({ purchases });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 module.exports = {
     courseRouter: courseRouter
